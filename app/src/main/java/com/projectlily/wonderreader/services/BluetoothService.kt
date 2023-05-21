9cdf2e2bc28243afb4a3a13b0607eee7965f3e37
Aric-prog <hernandoaric@gmail.com>
2023-05-22 01:34:43 +1000

    initial dev commit. Added stub files for services

---
+++ app/src/main/java/com/projectlily/wonderreader/services/BluetoothService.kt
@@ -0,0 +1,13 @@
+package com.projectlily.wonderreader.services
+
+import com.projectlily.wonderreader.types.JsonEvent
+
+class BluetoothService {
+    fun send(jsonEvent: JsonEvent) {
+        return
+    }
+
+    fun onReceive() {
+        return;
+    }
+}
---
+++ app/src/main/java/com/projectlily/wonderreader/types/JsonEvent.kt
@@ -0,0 +1,8 @@
+package com.projectlily.wonderreader.types
+
+import org.json.JSONObject
+
+// Contains event and data that will be sent to the wonder device
+class JsonEvent(event: EventType, data: JSONObject) {
+
+}
